3f578962fe4df0149af460d5db1f7b1fc3ec7307
Sheldon Yang <sheldon@excelsia.io>
2019-08-26 12:56:10 +0800

    add EqualsAndHashCode to Minting TX
    add this Annotation because  Minting TX is implemented without calling superclass.

--- src/v/systems/transaction/MintingTransaction.java
+++ src/v/systems/transaction/MintingTransaction.java
@@ -1,8 +1,10 @@
 package v.systems.transaction;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class MintingTransaction extends BasicTransaction {
     protected String recipient;
     protected Long amount;
